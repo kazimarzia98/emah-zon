ff4e68122efde358141b4326a7b8c35f15febc25
Kazimarzia98 <Kazimarzia98@gmail.com>
2023-12-30 18:33:01 +0600

    handleCart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const { img, name, price, seller, ratings } = props.product;
+    const { img, name, price, seller, ratings} = props. product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +14,8 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Ratings: {ratings} star</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={() => handleAddToCart(props.product )} 
+            className='btn-cart'>Add to cart</button>
         </div>
     );
 };
